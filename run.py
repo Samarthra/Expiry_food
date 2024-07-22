a2487b50572d04c1000c8149202484292c7f2e37
samarthra <sudhakarsudhi65@gmail.com>
2024-07-22 22:35:15 +0530

    tuned model

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load('best_model.pkl')
+model = joblib.load('best_gradient_boosting_model.pkl')
 
 @app.route('/')
 def home():
